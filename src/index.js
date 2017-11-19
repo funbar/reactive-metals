a612f5193d5e98fe6f0dc085fc55b5c7e8f80302
Rich <rkim43@uic.edu>
2017-11-19 01:36:02 +0000

    add configs

---
+++ src/index.js
@@ -0,0 +1,3 @@
+let message = 'Test';
+
+console.log(message);
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = {
     devtool: 'inline-source-map', // line numbers for debugging
     entry: [
-        'webpack-dev-server/client?http://127.0.0.1:8080/', // defines where webpack looks for the files and hot reloading
+        'webpack-dev-server/client?http://127.0.0.1:4444/', // defines where webpack looks for the files and hot reloading
         'webpack/hot/only-dev-server', // dev server
         './src'
     ],
@@ -13,20 +13,20 @@ module.exports = {
         filename: 'bundle.js' // standard name
     },
     resolve: {
-        modulesDirectories: ['node_modules', 'src'], // where webpack looks for the files to bundle together
-        extensions: ['', '.js']
+        modules: ['node_modules', 'src'], // where webpack looks for the files to bundle together
+        extensions: ['.js']
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.jsx?$/,
-                exclude: /node_module/,
-                loader: ['react-hot', 'babel?presets[]=react,presets[]=es2015'] // react-hot is for hot reloading and babel is for es6 syntax
+                exclude: /node_modules/,
+                loaders: ['babel-loader'] // react-hot is for hot reloading and babel is for es6 syntax
             }
         ]
     },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(), // live reloading
-        new webpack.NoErrorsPlugin() // fix errors before finishing compiling
-    ]
-}
+    // plugins: [
+    //     new webpack.HotModuleReplacementPlugin(), // live reloading
+    //     new webpack.NoErrorsPlugin() // fix errors before finishing compiling
+    // ]
+};
